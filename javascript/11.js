70accd898db3f4c33910e8ea26f154422f0b5592
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-27 23:27:58 -0300

    Update 11.js

--- javascript/11.js
+++ javascript/11.js
@@ -1,1 +1,9 @@
-console.log('O console funcionou corretamente');
+var vel = 78;
+
+console.log(`A sua velocidade é de ${vel}`)
+
+if (vel > 60) {
+    console.log('Voçê foi multado')
+}
+
+console.log('Diriga com segurança')
