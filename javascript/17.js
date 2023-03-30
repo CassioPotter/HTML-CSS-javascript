4edcefd2d8775ecd16cbc310cf2b455bfcc5d0d7
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-30 20:49:46 -0300

    Create 17.js

---
+++ javascript/17.js
@@ -0,0 +1,12 @@
+let amigo = {
+    nome: 'cassio',
+    sexo: 'm',
+    peso: 92,
+    engordar(p = 0) {
+        console.log('engordou')
+        this.peso += p
+    }
+}
+
+amigo.engordar(5)
+console.log(amigo.peso)
