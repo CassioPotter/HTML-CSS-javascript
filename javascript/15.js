94a2ccd08929ee20ebda0005b2598bf004906fda
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-30 10:49:18 -0300

    Create 15.js

---
+++ javascript/15.js
@@ -0,0 +1,13 @@
+let num = [0, 8, 6, 4] 
+
+console.log(num)
+num.push(1)
+console.log(num)
+console.log(`Tem ${num.length} valores`)
+num.sort()
+console.log(num)
+
+for (let pos = 0; pos < num.length; pos = pos + 1){
+    console.log (`A posiçao ${pos} tem o valor ${num[pos]}`)
+}
+
