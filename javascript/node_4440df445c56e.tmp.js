a784a5e9dfc69200bb411e84d847d8bee07906d2
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-30 10:49:32 -0300

    Create node_4440df445c56e.tmp.js

---
+++ javascript/node_4440df445c56e.tmp.js
@@ -0,0 +1,12 @@
+let num = [0, 8, 6, 4] 
+
+console.log(num)
+num.push(1)
+console.log(num)
+console.log(`Tem ${num.length} valores`)
+num.sort()
+console.log(num)
+
+for (let pos = 0; pos, num.length; pos++){
+    console.log`A posiçao ${pos} tem o valor ${num[pos]}`
+}
